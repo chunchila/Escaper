82b46490bb03897c235ca50ae7af89d2d5a56e91
chunchila <aka.rozin@gmail.com>
2016-11-08 18:40:24 +0200

    sdsd

---
+++ cli.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+nowTime = (datetime.now().strftime('%H:%M'))
+someTime = ('18:45')
+
+s1 = someTime.split(':')
+s2 = nowTime.split(':')
+
+if s1[0] == s2[0]:
+    print(int(s1[1]) - int(s2[1]))
+
+    print(nowTime)
