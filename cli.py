d26e368e95a62b00ba1ff8a6e63c545a13e34ed6
chunchila <aka.rozin@gmail.com>
2016-11-08 18:48:52 +0200

    dfd

--- cli.py
+++ cli.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
 nowTime = (datetime.now().strftime('%H:%M'))
-someTime = ('18:15')
+someTime = ('18:50')
 
 s1 = someTime.split(':')
 s2 = nowTime.split(':')
 
 if s1[0] == s2[0]:
-    print(int(s1[1]) - int(s2[1]))
+    print(int(s1[1]) - int(s2[1]) <=10)
 
     print(nowTime)
